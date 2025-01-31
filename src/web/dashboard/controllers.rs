39f2225589c7a26308514f3f429a6b3bf301aa97
Alex Szabo <kreatemore@gmail.com>
2025-01-31 22:43:57 +0100

    demo SSE implementation
    Closes #3

--- src/web/dashboard/controllers.rs
+++ src/web/dashboard/controllers.rs
@@ -1,6 +1,12 @@
 use crate::{models::_entities::users::Model, web::security::CspNonce};
-use axum::Extension;
+use axum::{
+    response::{sse::Event, Sse},
+    Extension,
+};
+use futures::stream::{self, Stream};
 use loco_rs::{controller::middleware::auth::JWTWithUser, prelude::*};
+use std::{convert::Infallible, time::Duration};
+use tokio::time::sleep;
 
 pub(super) async fn greet(
     auth: JWTWithUser<Model>,
@@ -8,4 +14,20 @@ pub(super) async fn greet(
     Extension(nonce): Extension<CspNonce>,
 ) -> Result<impl IntoResponse> {
     super::views::dashboard(&v, &auth.user, nonce.value.as_str())
+}
+
+pub(super) async fn keep_updating(
+    _auth: JWTWithUser<Model>,
+) -> Sse<impl Stream<Item = Result<Event, Infallible>>> {
+    let stream = stream::unfold(0, |count| async move {
+        sleep(Duration::from_secs(2)).await;
+        let event = Event::default().data(format!("Message #{count}"));
+        Some((Ok(event), count + 1))
+    });
+
+    Sse::new(stream).keep_alive(
+        axum::response::sse::KeepAlive::new()
+            .interval(Duration::from_secs(1))
+            .text("keep-alive-text"),
+    )
 }
--- src/web/dashboard/router.rs
+++ src/web/dashboard/router.rs
@@ -1,7 +1,9 @@
 use loco_rs::prelude::*;
 
-use super::controllers::greet;
+use super::controllers::{greet, keep_updating};
 
 pub fn routes() -> Routes {
-    Routes::new().add("/", get(greet))
+    Routes::new()
+        .add("/", get(greet))
+        .add("/events", get(keep_updating))
 }
